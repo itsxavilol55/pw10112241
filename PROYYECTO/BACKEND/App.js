9c422e5779a2372334a1baa28c4e95989a8d38bc
itsxavilol55 <l20170727@culiacan.tecnm.mx>
2024-04-29 10:56:34 -0700

    peticion clientes por id

--- PROYYECTO/BACKEND/App.js
+++ PROYYECTO/BACKEND/App.js
@@ -11,19 +11,36 @@ let conexion = mysql.createConnection(
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'pwdata'
+        database: 'pwdata',
+        port: '8080'
     }
 );
-
 conexion.connect(function(error){
     if(error)
-        throw error;
-    console.log('conectado a la base de datos')
+        console.log('hubo un error');
+    else
+        console.log('conectado a la base de datos');
+});
+app.get('/api/clientes/',(req,res)=>{
+    conexion.query('select * from clientes',(error,fila)=>{
+        if(error)
+        console.log('hubo un error al pedir datos');
+    else
+        res.send(fila)
+    });
+});
+app.get('/api/clientes/:id',(req,res)=>{
+    conexion.query('select * from clientes where id=?',[req.params.id],
+    (error,fila)=>{
+        if(error)
+        console.log('hubo un error al pedir datos');
+    else
+        res.send(fila)
+    });
 });
 app.get("/",function(req,res){
     res.send('rutas de inicio');
 });
-
 app.listen(3000, function(){
     console.log('Servidor listo');
 });
