a56e0f9de8d94b6d04730526d55315681115a8dc
itsxavilol55 <l20170727@culiacan.tecnm.mx>
2024-04-30 10:55:33 -0700

    peticion post

--- PROYYECTO/BACKEND/App.js
+++ PROYYECTO/BACKEND/App.js
@@ -38,6 +38,35 @@ app.get('/api/clientes/:id',(req,res)=>{
         res.send(fila)
     });
 });
+app.delete('/api/clientes/:id',(req,res)=>{
+    conexion.query('delete from clientes where id=?',[req.params.id],
+    (error,fila)=>{
+        if(error)
+        console.log('hubo un error al pedir datos');
+    else
+        res.send(fila)
+    });
+});
+app.post('/api/clientes',(req,res)=>{
+    let data = {
+        id: req.body.id,
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        direccion: req.body.direccion,
+        telefono: req.body.telefono,
+        rfc: req.body.rfc,
+        curp: req.body.curp,
+        cp: req.body.cp
+    }
+    let sql = "insert into clientes set ?";
+    conexion.query(sql,data,
+    (error,resultado)=>{
+        if(error)
+        console.log('hubo un error al pedir datos');
+    else
+        res.send(resultado)
+    });
+});
 app.get("/",function(req,res){
     res.send('rutas de inicio');
 });
