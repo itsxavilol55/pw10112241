91af634e8a119af132827f308532526ee310cb82
itsxavilol55 <l20170727@culiacan.tecnm.mx>
2024-05-08 11:04:50 -0700

    menu bootstrap

---
+++ PROYYECTO/FRONTEND/TIENDA/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import MenuView from './views/MenuView.vue';
+</script>
+
+<template>
+  <header>
+    <MenuView></MenuView>
+  </header>
+</template>
+
---
+++ PROYYECTO/FRONTENDREACT/tienda/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import MenuView from '../src/components/MenuView'
+function App() {
+  return( 
+  <>
+    <MenuView></MenuView>
+  </>);
+}
+export default App;
