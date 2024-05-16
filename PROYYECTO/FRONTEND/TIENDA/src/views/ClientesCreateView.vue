2ca96d0d15630cfa484ca1e26bd95e095b833e63
itsxavilol55 <l20170727@culiacan.tecnm.mx>
2024-05-16 10:53:43 -0700

    agregar cliente

--- PROYYECTO/FRONTEND/TIENDA/src/views/ClientesCreateView.vue
+++ PROYYECTO/FRONTEND/TIENDA/src/views/ClientesCreateView.vue
@@ -3,6 +3,9 @@
     <div class="card">
       <div class="card-header">
         <h4>agregar clientes</h4>
+        <div v-if="mensaje==1" class="alert alert-success" role="alert">
+            Datos guardados con exito
+        </div>
       </div>
       <div class="card-body">
         <div class="mb-3">
@@ -38,17 +41,19 @@
             <input type="text" class="form-control" v-model="model.cliente.cp">
         </div>
         <div class="mb-3">
-            <button class="btn btn-primary">Guardar</button>
+            <button class="btn btn-primary" @click="GuardarCliente()">Guardar</button>
         </div> 
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios';
 export default{
     name: "ClientesCreate",
     data(){
         return{
+            mensaje: 0,
             model:{
                 cliente:{
                     id: '',
@@ -62,6 +67,27 @@ export default{
                 }
             }
         }
+    },
+    methods: {
+       GuardarCliente(){
+        axios.post('http://localhost:3000/api/clientes', this.model.cliente)
+            .then(res=>{
+                if(res.data.affectedRows == 1){
+                    this.model.cliente = {
+                        id: '',
+                        nombre:'',
+                        apellido:'',
+                        direccion:'',
+                        telefono:'',
+                        rfc:'',
+                        curp:'',
+                        cp:''
+                    }
+                    this.mensaje=1;
+                }
+
+            });
+       }
     }
 }
 </script>
